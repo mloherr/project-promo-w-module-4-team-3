75901418d36930ff61bb96b0ba974124ca3c5021
Noelia <abascalcabreran@gmail.com>
2024-04-03 15:29:32 +0200

    changes vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "/project-promo-w-module-3-team-3/",
+  plugins: [react()],
+  base: '/project-promo-w-module-4-team-3/',
 });
