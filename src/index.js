a263063670a7419048d8c02dc1b61fc5d28890b5
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-04-16 13:00:52 +0200

    Add render and env

--- src/index.js
+++ src/index.js
@@ -1,82 +1,84 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2/promise');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 const server = express();
 server.use(cors());
-server.use(express.json({ limit: '50mb' }));
-server.use(express.urlencoded({ limit: '50mb' }));
-server.set('view engine', 'ejs');
+server.use(express.json({ limit: "50mb" }));
+server.use(express.urlencoded({ limit: "50mb" }));
+server.set("view engine", "ejs");
+require("dotenv").config();
 
 async function getDBConnection() {
-  const connection = await mysql.createConnection({
-    host: 'sql.freedb.tech',
-    user: 'freedb_admin_choris',
-    password: 'u&3W785vX6v?FM#',
-    database: 'freedb_choripopins_project',
-  });
-  connection.connect();
-  return connection;
+	const connection = await mysql.createConnection({
+		host: "sql.freedb.tech",
+		user: "freedb_admin_choris",
+		password: `${process.env.password}`,
+		database: `${process.env.database}`,
+	});
+	connection.connect();
+	return connection;
 }
 
-const port = 5001;
+const port = `${process.env.port}`;
 server.listen(port, () => {
-  console.log('Server is running on port ' + port);
+	console.log("Server is running on port " + port);
 });
 
-server.get('/api/projects', async (req, res) => {
-  const connection = await getDBConnection();
-  const sql = 'SELECT * FROM author, projects WHERE idAuthor = fk_idAuthor';
-  const [projectsResults] = await connection.query(sql);
-  connection.end();
-  res.status(200).json({
-    status: 'success',
-    projects: projectsResults,
-  });
+server.get("/api/projects", async (req, res) => {
+	const connection = await getDBConnection();
+	const sql = "SELECT * FROM author, projects WHERE idAuthor = fk_idAuthor";
+	const [projectsResults] = await connection.query(sql);
+	connection.end();
+	res.status(200).json({
+		status: "success",
+		projects: projectsResults,
+	});
 });
 
-server.post('/api/newproject', async (req, res) => {
-  const connection = await getDBConnection();
-  const authorQuerySQL = 'INSERT INTO author (author, job, photo) VALUES (?,?,?)';
-  const [authorResult] = await connection.query(authorQuerySQL, [
-    req.body.autor,
-    req.body.job,
-    req.body.photo,
-  ]);
+server.post("/api/newproject", async (req, res) => {
+	const connection = await getDBConnection();
+	const authorQuerySQL =
+		"INSERT INTO author (author, job, photo) VALUES (?,?,?)";
+	const [authorResult] = await connection.query(authorQuerySQL, [
+		req.body.autor,
+		req.body.job,
+		req.body.photo,
+	]);
 
-  const sql =
-    'INSERT INTO projects (name, slogan, descr, technologies, image, urlGithub, urlDemo, fk_idAuthor) VALUES (?,?,?,?,?,?,?,?)';
-  const [projectResult] = await connection.query(sql, [
-    req.body.name,
-    req.body.slogan,
-    req.body.desc,
-    req.body.technologies,
-    req.body.image,
-    req.body.repo,
-    req.body.demo,
-    authorResult.insertId,
-  ]);
-  res.status(201).json({
-    sucess: true,
-    idProject: projectResult.insertId,
-    idAuthor: authorResult.insertId,
-    cardURL: `http://localhost:5001/detail/${projectResult.insertId}`,
-  });
+	const sql =
+		"INSERT INTO projects (name, slogan, descr, technologies, image, urlGithub, urlDemo, fk_idAuthor) VALUES (?,?,?,?,?,?,?,?)";
+	const [projectResult] = await connection.query(sql, [
+		req.body.name,
+		req.body.slogan,
+		req.body.desc,
+		req.body.technologies,
+		req.body.image,
+		req.body.repo,
+		req.body.demo,
+		authorResult.insertId,
+	]);
+	res.status(201).json({
+		sucess: true,
+		idProject: projectResult.insertId,
+		idAuthor: authorResult.insertId,
+		cardURL: `${process.env.URL}/detail/${projectResult.insertId}`,
+	});
 });
 
-server.get('/detail/:idProject', async (req, res) => {
-  const { idProject } = req.params;
-  const connection = await getDBConnection();
-  const sqlQuery =
-    'SELECT * FROM projects, author WHERE projects.fk_idAuthor = author.idAuthor AND projects.idProject = ?';
-  const [result] = await connection.query(sqlQuery, [idProject]);
-  connection.end();
-  res.render('detail', { project: result[0] });
+server.get("/detail/:idProject", async (req, res) => {
+	const { idProject } = req.params;
+	const connection = await getDBConnection();
+	const sqlQuery =
+		"SELECT * FROM projects, author WHERE projects.fk_idAuthor = author.idAuthor AND projects.idProject = ?";
+	const [result] = await connection.query(sqlQuery, [idProject]);
+	connection.end();
+	res.render("detail", { project: result[0] });
 });
 
-const staticServer = './web';
+const staticServer = "./web";
 server.use(express.static(staticServer));
 
-const pathServerPublicStyles = './src/public-css';
+const pathServerPublicStyles = "./src/public-css";
 server.use(express.static(pathServerPublicStyles));
-server.use(express.static('public'));
+server.use(express.static("public"));
--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,75 +1,80 @@
-import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import '../scss/App.scss';
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "../scss/App.scss";
 
-import LandingPage from './LandingPage';
-import MainPage from './MainPage';
-import localStorage from '../services/localStorage';
-import projectApi from '../services/projectsApi';
+import LandingPage from "./LandingPage";
+import MainPage from "./MainPage";
+import localStorage from "../services/localStorage";
+import projectApi from "../services/projectsApi";
 
 const App = () => {
-  const infoDefault = {
-    name: '',
-    slogan: '',
-    repo: '',
-    demo: '',
-    technologies: '',
-    desc: '',
-    autor: '',
-    job: '',
-    image: 'https://i.blogs.es/25fd38/disney-mary-poppins-critica-1964/450_1000.jpg',
-    photo: 'https://img.rtve.es/imagenes/mary-poppins-dificultades-su-rodaje/1641918077286.jpg',
-  };
-  const [projectInfo, setProjectInfo] = useState(localStorage.get('project') || infoDefault);
-  const [url, setUrl] = useState('');
-  const [projectsApi, setProjectsApi] = useState([]);
+	const infoDefault = {
+		name: "",
+		slogan: "",
+		repo: "",
+		demo: "",
+		technologies: "",
+		desc: "",
+		autor: "",
+		job: "",
+		image:
+			"https://i.blogs.es/25fd38/disney-mary-poppins-critica-1964/450_1000.jpg",
+		photo:
+			"https://img.rtve.es/imagenes/mary-poppins-dificultades-su-rodaje/1641918077286.jpg",
+	};
+	const [projectInfo, setProjectInfo] = useState(
+		localStorage.get("project") || infoDefault
+	);
+	const [url, setUrl] = useState("");
+	const [projectsApi, setProjectsApi] = useState([]);
 
-  useEffect(() => {
-    projectApi.getProjectsFromApi().then((response) => {
-      setProjectsApi(response.projects);
-    });
-  }, []);
+	useEffect(() => {
+		projectApi.getProjectsFromApi().then((response) => {
+			setProjectsApi(response.projects);
+		});
+	}, []);
 
-  const handleCreateProject = () => {
-    projectApi
-      .postNewProject(projectInfo)
-      .then((response) => response.json())
-      .then((result) => {
-        setUrl(result.cardURL);
-      })
-      .catch((error) => console.log(error));
-  };
+	const handleCreateProject = () => {
+		projectApi
+			.postNewProject(projectInfo)
+			.then((response) => response.json())
+			.then((result) => {
+				setUrl(result.cardURL);
+			})
+			.catch((error) => console.log(error));
+	};
 
-  const handleProjectInfo = (value, id) => {
-    setProjectInfo({ ...projectInfo, [id]: value });
-    localStorage.set('project', projectInfo);
-  };
-  const handleReset = () => {
-    setProjectInfo(infoDefault);
-    setUrl('');
-    localStorage.clear();
-  };
-  return (
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/MainPage//*"
-          element={
-            <MainPage
-              projectInfo={projectInfo}
-              cardUrl={url}
-              onChangeProjectInfo={handleProjectInfo}
-              updateAvatar={handleProjectInfo}
-              onClickSave={handleCreateProject}
-              onClickReset={handleReset}
-              projectsApi={projectsApi}
-            />
-          }
-        />
-      </Routes>
-    </div>
-  );
+	const handleProjectInfo = (value, id) => {
+		setProjectInfo({ ...projectInfo, [id]: value });
+		localStorage.set("project", projectInfo);
+	};
+	const handleReset = () => {
+		setProjectInfo(infoDefault);
+		setUrl("");
+		localStorage.clear();
+	};
+
+	return (
+		<div className="container">
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route
+					path="/MainPage//*"
+					element={
+						<MainPage
+							projectInfo={projectInfo}
+							cardUrl={url}
+							onChangeProjectInfo={handleProjectInfo}
+							updateAvatar={handleProjectInfo}
+							onClickSave={handleCreateProject}
+							onClickReset={handleReset}
+							projectsApi={projectsApi}
+						/>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
