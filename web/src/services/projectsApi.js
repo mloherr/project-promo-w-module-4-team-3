3ccb8ac7a28252e9cdbf8362726339a7816f635e
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-04-16 13:09:39 +0200

    Add dotenv

--- web/src/services/projectsApi.js
+++ web/src/services/projectsApi.js
@@ -1,22 +1,24 @@
+const URL = "https://choripopins-project.onrender.com";
+
 const getProjectsFromApi = () => {
-  return fetch('//localhost:5001/api/projects')
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`${URL}/api/projects`)
+		.then((response) => response.json())
+		.then((data) => {
+			return data;
+		});
 };
 
 const postNewProject = (bodyParams) => {
-  return fetch('//localhost:5001/api/newproject', {
-    method: 'POST',
-    body: JSON.stringify(bodyParams),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+	return fetch(`${URL}/api/newproject`, {
+		method: "POST",
+		body: JSON.stringify(bodyParams),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 };
 
 export default {
-  getProjectsFromApi: getProjectsFromApi,
-  postNewProject: postNewProject,
+	getProjectsFromApi: getProjectsFromApi,
+	postNewProject: postNewProject,
 };
