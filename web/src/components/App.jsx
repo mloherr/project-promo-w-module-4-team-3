9287c756b6833b046b043766444419ff58608d2f
Bea Ruf <bearm12493@gmail.com>
2024-04-20 11:59:00 +0100

    links and minor fixes

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -8,74 +8,77 @@ import localStorage from "../services/localStorage";
 import projectApi from "../services/projectsApi";
 
 const App = () => {
-	const infoDefault = {
-		name: "",
-		slogan: "",
-		repo: "",
-		demo: "",
-		technologies: "",
-		desc: "",
-		autor: "",
-		job: "",
-		image:
-			"https://i.blogs.es/25fd38/disney-mary-poppins-critica-1964/450_1000.jpg",
-		photo:
-			"https://img.rtve.es/imagenes/mary-poppins-dificultades-su-rodaje/1641918077286.jpg",
-	};
-	const [projectInfo, setProjectInfo] = useState(
-		localStorage.get("project") || infoDefault
-	);
-	const [url, setUrl] = useState("");
-	const [projectsApi, setProjectsApi] = useState([]);
+  const infoDefault = {
+    name: "",
+    slogan: "",
+    repo: "",
+    demo: "",
+    technologies: "",
+    desc: "",
+    autor: "",
+    job: "",
+    image:
+      "https://i.blogs.es/25fd38/disney-mary-poppins-critica-1964/450_1000.jpg",
+    photo:
+      "https://img.rtve.es/imagenes/mary-poppins-dificultades-su-rodaje/1641918077286.jpg",
+  };
+  const [projectInfo, setProjectInfo] = useState(
+    localStorage.get("project") || infoDefault
+  );
+  const [url, setUrl] = useState("");
+  const [projectsApi, setProjectsApi] = useState([]);
 
-	useEffect(() => {
-		projectApi.getProjectsFromApi().then((response) => {
-			setProjectsApi(response.projects);
-		});
-	}, []);
+  useEffect(() => {
+    projectApi.getProjectsFromApi().then((response) => {
+      setProjectsApi(response.projects);
+    });
+  }, []);
 
-	const handleCreateProject = () => {
-		projectApi
-			.postNewProject(projectInfo)
-			.then((response) => response.json())
-			.then((result) => {
-				setUrl(result.cardURL);
-				console.log(result);
-			})
-			.catch((error) => console.log(error));
-	};
+  useEffect(() => {
+    localStorage.set("project", projectInfo);
+  }, [projectInfo]);
 
-	const handleProjectInfo = (value, id) => {
-		setProjectInfo({ ...projectInfo, [id]: value });
-		localStorage.set("project", projectInfo);
-	};
-	const handleReset = () => {
-		setProjectInfo(infoDefault);
-		setUrl("");
-		localStorage.clear();
-	};
+  const handleCreateProject = () => {
+    projectApi
+      .postNewProject(projectInfo)
+      .then((response) => response.json())
+      .then((result) => {
+        setUrl(result.cardURL);
+        console.log(result);
+      })
+      .catch((error) => console.log(error));
+  };
 
-	return (
-		<div className="container">
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route
-					path="/MainPage//*"
-					element={
-						<MainPage
-							projectInfo={projectInfo}
-							cardUrl={url}
-							onChangeProjectInfo={handleProjectInfo}
-							updateAvatar={handleProjectInfo}
-							onClickSave={handleCreateProject}
-							onClickReset={handleReset}
-							projectsApi={projectsApi}
-						/>
-					}
-				/>
-			</Routes>
-		</div>
-	);
+  const handleProjectInfo = (value, id) => {
+    setProjectInfo({ ...projectInfo, [id]: value });
+  };
+  const handleReset = () => {
+    setProjectInfo(infoDefault);
+    setUrl("");
+    localStorage.clear();
+  };
+
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/MainPage//*"
+          element={
+            <MainPage
+              projectInfo={projectInfo}
+              cardUrl={url}
+              onChangeProjectInfo={handleProjectInfo}
+              updateAvatar={handleProjectInfo}
+              onClickSave={handleCreateProject}
+              onClickReset={handleReset}
+              projectsApi={projectsApi}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
